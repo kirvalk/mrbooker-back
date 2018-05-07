11e6eff620c306ce350a26e5c770e5d485972979
ShelpakovSergey <shelpakovsergey@gmail.com>
2018-05-07 20:13:30 +0700

    add project files

--- packages/core/core.js
+++ packages/core/core.js
@@ -1,40 +1,26 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const error = require('../error/error');
+const tasksRoutes = require('../tasks/tasks');
 
-var tasksRoutes = require('../tasks/tasks');
-var usersRoutes = require('../users/users');
+const app = express();
 
-var app = express();
-
-// uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRoutes);
-app.use('/tasks', tasksRoutes);
+app.use('/api/v001/tasks', tasksRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res) => {
+  res.json({ status: 'BAD_REQUEST', messages: [error({ code: 'BAD_REQUEST' })] });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send({ status: 'FAIL'});
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.json({ status: 'FAIL', messages: [error({ code: err.message })] });
 });
 
 module.exports = app;
