dd78fab1c500807592fc06650e9007a1327a9d19
Kirill Karagedov <karagedovk@gmail.com>
2018-05-08 09:17:03 +0700

    fixed var names

--- packages/db/db.js
+++ packages/db/db.js
@@ -4,7 +4,7 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-db.defaults({ tasks: [] })
+db.defaults({ rooms: [] })
   .write();
 
 module.exports = db;
